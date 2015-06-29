77aad1084356e2bfd5df7f403218a31e93837c58
Jolie Hou <lhou@taplion.com>
2015-06-29 19:42:32 +0800

    Initial Commit

---
+++ PetHome2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PetHome2
+//
+//  Created by 亮亮 侯 on 6/29/15.
+//  Copyright (c) 2015 亮亮 侯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
